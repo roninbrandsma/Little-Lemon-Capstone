60892a7e60055d8ed7bee0f67c9a5bc14eb1c59d
roninbrandsma <brandsmaronin57@gmail.com>
2025-04-05 01:19:05 +0100

    progress on layout

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -3,33 +3,33 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
-import { Image } from "react-bootstrap";
 
+import "../../variables.scss";
 import "./header.scss"
-
-
 
 const Header = () => {
     return(
         <>
-        <Navbar>
-            <Container>
-            <Navbar.Brand href="/" className="headertext">
-                <Image
-                    src="../assets/Logo.jpg"
-                    width={70}
-                    height={30}
-                />
-            </Navbar.Brand>
-                <Nav className="navlinks">
-                    <NavLink to="/portfolio" className="headertext headerlink" >Portfolio</NavLink>
-                    <NavLink to="/contact" className="headertext headerlink">Contact</NavLink>
-                </Nav>
-            </Container>
-        </Navbar>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="/" className="headertext">
+                        <img
+                            src="../../../public/assets/Logo.jpg"
+                            alt=""
+                            height={40}
+                            width={148}
+                        />
+                    </Navbar.Brand>
+                    <Nav className="navlinks">
+                        <NavLink to="/portfolio" className="headertext headerlink" >Portfolio</NavLink>
+                        <NavLink to="/contact" className="headertext headerlink">Contact</NavLink>
+                    </Nav>
+                </Container>
+            </Navbar>
         </>
     )
-
 }
+
+
 
 export default Header
--- src/Components/HomePage/homepage.jsx
+++ src/Components/HomePage/homepage.jsx
@@ -1,18 +1,50 @@
-import { Image } from "react-bootstrap"
+import { Container, Row, Col, Button, Card } from "react-bootstrap";
+
 
 const HomePage = () => {
     return(
         <>
             <div className='heroSectionDiv'>
-                <h1>Little Lemon</h1>
-                <h5>Chicago</h5>
-                <p></p>
-                <button>Reserve a Table</button>
-                <Image className="heroImage"
-                    src="../assets/restaurant.jpg"
-                    width={300}
-                    height={300}
-                />
+                <Container>
+                    <Row>
+                        <Col className="heroContInfo">
+                            <h4>Little Lemon</h4>
+                            <p>Chicago</p>
+                            <p className="heroPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget mattis orci. Praesent ultrices, nunc in accumsan elementum, lacus ipsum lacinia massa, vitae feugiat ante odio non diam. Donec velit lectus, eleifend in turpis nec, suscipit tincidunt sem.</p>
+                            <Button className="reserveBtn" variant="custom" >Reserve a Table</Button>
+                        </Col>
+                        <Col className="heroContImg">
+                            <img className="restImage" src="../../assets/restaurant.jpg" alt=""/>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <div className="specialSectionDiv">
+                <Container>
+                    <Row>
+                        <Col className="specialSectionCol">
+                            <Card className="specialSectionCard"  variant="light">
+                                <Card.Img src="../../assets/bruchetta.svg"/>
+                                <Card.Text></Card.Text>
+                            </Card>
+                        </Col>
+                        <Col className="specialSectionCol">
+                            <Card className="specialSectionCard"></Card>
+                        </Col>
+                        <Col className="specialSectionCol">
+                            <Card className="specialSectionCard"></Card>
+                        </Col>
+                        <Col className="specialSectionCol">
+                            <Card className="specialSectionCard"></Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <div className="reviewSection">
+
+            </div>
+            <div className="aboutSection">
+
             </div>
         </>
     )
